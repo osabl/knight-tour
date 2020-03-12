946d8a6d8755a919b673f1e0840d3ed8ea29e505
osabl <andrii.osabliuk@gmail.com>
2020-03-12 17:55:55 +0200

    split into seperate files

---
+++ src/js/chessBoard.js
@@ -0,0 +1,49 @@
+export default class ChessBoard { 
+  constructor (width = 8, height = 8) {
+    this.width = width
+    this.height = height
+    this.area = this.width * this.height
+    this.history = []
+    this.table = []
+
+    for (let y = 0; y < height; y++) {
+      let row = []
+
+      for (let x = 0; x < width; x++) {
+        const cell = {
+          x,
+          y,
+          used: false
+        }
+
+        row.push(cell)
+      }
+
+      this.table.push(row)
+    }
+  }
+
+  getPossibleMoves(movements, x, y) {
+    const cell = this.getCell(x, y)
+    const possibleMoves = []
+
+    for (const key in movements) {
+      const x = cell.x + movements[key].x
+      const y = cell.y + movements[key].y
+
+      try {
+        if (!this.table[y][x].used) {
+          possibleMoves.push(this.table[y][x])
+        }
+      } catch {
+        continue
+      }
+    }
+
+    return possibleMoves
+  }
+
+  getCell(x, y) {
+    return this.table[y][x]
+  }
+}
--- src/js/knight.js
+++ src/js/knight.js
@@ -1,78 +1,9 @@
-class ChessBoard { 
-  constructor (width = 8, height = 8) {
-    this.width = width
-    this.height = height
-    this.area = this.width * this.height
-    this.history = []
-    this.table = []
-
-    for (let y = 0; y < height; y++) {
-      let row = []
-
-      for (let x = 0; x < width; x++) {
-        const cell = {
-          x,
-          y,
-          used: false,
-
-          stay() {
-            if (!this.used) {
-              this.used = true
-              return true
-            } else {
-              return false
-            }
-          },
-
-          undo () {
-            if (this.used) {
-              this.used = false
-              return true
-            } else {
-              return false
-            }
-          }
-        }
-
-        row.push(cell)
-      }
-
-      this.table.push(row)
-    }
-  }
-
-  possibleMoves(movements, x, y) {
-    const cell = this.getCell(x, y)
-    let possible = 0
-
-    for (const key in movements) {
-      const x = cell.x + movements[key].x
-      const y = cell.y + movements[key].y
-
-      try {
-        if (!this.table[y][x].used) {
-          possible++
-        }
-      } catch {
-        continue
-      }
-    }
-
-    return possible
-  }
-
-  getCell(x, y) {
-    return this.table[y][x]
-  }
-}
-
-class Knight {
+export default class Knight {
   constructor (board, x = 0, y = 0) {
     this.x = x
     this.y = y
     this.board = board
 
-      
     if (board.width >= x && board.height >= y) {
       this.move({ x, y, description: 'start position' })
     } else {
@@ -91,54 +22,11 @@ class Knight {
     LU: { x: -2, y:  1, description: 'left up'    },
   }
 
- 
-
-  move ({ x, y, description}) {
+  move ({ x, y, description }) {
     this.x += x
     this.y += y
 
-    this.board.table[y][x].stay()
-
+    this.board.table[y][x].used = true
     this.board.history.push({ x, y, description })
-  }
-}
-
-
-// ----- FOR TESTING -----
-
-const chessBoard = new ChessBoard()
-const knight = new Knight(chessBoard)
-printBoard(chessBoard)
-printHistory(chessBoard)
-
-setTimeout(() => {
-  knight.move(Knight.movement.UR)
-}, 2000)
-setTimeout(() => {
-  printBoard(chessBoard)
-}, 5000)
-setTimeout(() => {
-  printHistory(chessBoard)
-}, 5000)
-
-
-function printBoard (chessBoard) {
-  console.log('===== PRINT BOARD =====')
-
-  for (let i = 0; i < chessBoard.table.length; i++) {
-    for (let j = 0; j < chessBoard.table[i].length; j++) {
-      setTimeout(() => {
-        console.log('----------')
-        console.log(chessBoard.table[i][j])
-      }, (i+j)*100)
-    }
-  }
-}
-
-function printHistory (chessBoard) {
-  console.log('===== PRINT BOARD HISTORY =====')
-
-  for (let move of chessBoard.history) {
-    console.log(move)
   }
 }
