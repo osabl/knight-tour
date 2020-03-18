7dd48ed568b616914c20e9098b3daf2321b4994d
osabl <andrii.osabliuk@gmail.com>
2020-03-18 16:10:18 +0200

    add canvas

--- src/js/view.js
+++ src/js/view.js
@@ -1,40 +1,66 @@
-export default class Board {
-  constructor (selector, options = {
-    rows: 8,
-    colums: 8,
-  }) {
-    const app = document.querySelector(selector)
+export default class View {
+  constructor (selector, options) {
+    this.canvas = document.createElement('canvas')
+    this.canvas.className = 'chessboard'
+    this.canvas.width = options.resolution
+    this.canvas.height = options.resolution
+    this.colums = options.colums
+    this.rows = options.rows
 
-    this.board = document.createElement('table')
-    this.board.className = 'chessboard'
-  
-    for (let i = 0; i < options.rows; i++) {
-      const row = document.createElement('tr')
-      
-      for (let j = 0; j < options.colums; j++) {
-        const cell = document.createElement('td')
-        cell.className = `cell ${(i + j) % 2 ? 'even' : 'odd'}`
-        
-        row.append(cell)
-      }
-  
-      this.board.append(row)
-    }
-
-    app.append(this.board)
+    this.renderBoard()
+    document.querySelector(selector).append(this.canvas)
   }
 
-  showWay(way, options = {
-    animation: true,
-    delay: 500,
-  }) {
-    if (options.animation) {
-      for (let i = 0; i < way.length; i++) {
-        setTimeout(() => {
-          this.board.rows[way[i].y].cells[way[i].x].innerText = i + 1
-        }, options.delay * i)    
+  renderBoard () {
+    const ctx = this.canvas.getContext('2d')
+    const size = this.canvas.width / (Number(this.colums) > Number(this.rows) ? this.colums : this.rows)
+
+    for (let i = 0; i < this.rows; i++) {
+      for (let j = 0; j < this.colums; j++) {
+        ctx.fillStyle = (i + j) % 2 ? '#B58863' : '#F0D9B5' // even or odd
+        ctx.fillRect(j * size, i * size, size, size)
       }
     }
-    
+  }
+
+  renderWay (way) {
+    const ctx = this.canvas.getContext('2d')
+    const size = this.canvas.width / (Number(this.colums) > Number(this.rows) ? this.colums : this.rows)
+    const radius = size / 4
+    ctx.lineWidth = size / 30
+
+    for (let i = 1; i < way.length; i++) {
+      setTimeout(() => {
+        ctx.fillStyle = i === 1 ? 'red' : 'black'
+
+        ctx.beginPath()
+        ctx.moveTo((way[i - 1].x * size) + size / 2, (way[i - 1].y * size) + size / 2)
+        ctx.lineTo((way[i].x * size) + size / 2, (way[i].y * size) + size / 2)
+        ctx.stroke()
+        ctx.closePath()
+
+        ctx.beginPath()
+        ctx.arc((way[i - 1].x * size) + size / 2, (way[i - 1].y * size) + size / 2, radius, 0, Math.PI * 2)
+        ctx.fill()
+        ctx.closePath()
+
+        ctx.fillStyle = 'green'
+
+        ctx.beginPath()
+        ctx.arc((way[i].x * size) + size / 2, (way[i].y * size) + size / 2, radius, 0, Math.PI * 2)
+        ctx.fill()
+        ctx.closePath()
+      }, i * 6400 / (this.colums * this.rows))
+    }
+  }
+
+  getSelectedCell (x, y) {
+    const ctx = this.canvas.getContext('2d')
+    const size = this.canvas.width / (Number(this.colums) > Number(this.rows) ? this.colums : this.rows)
+    const radius = size / 4
+
+    const newX = Math.floor(x / size)
+    const newY = Math.floor(y / size)
+    return { x: newX, y: newY }
   }
 }
